83c9435aeb6a33d17bd1b361f6357d9ad6701888
Elias Prost <elias.prost@inventa.shop>
2025-03-09 14:15:36 -0300

    update config doc

--- src/config.py
+++ src/config.py
@@ -5,17 +5,21 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     """
     This class defines the settings for the application. The settings are
-    loaded from the environment files specified in the Config class:
-        - .env
+    loaded from the environment files specified in the Config class: .env
 
     Documentation:
     - https://docs.pydantic.dev/latest/concepts/pydantic_settings/
 
     Attributes:
+    - MIN_VIDEO_DURATION: The minimum duration of the video in seconds.
     - REDDIT_CLIENT_ID: The client ID of the Reddit application.
     - REDDIT_CLIENT_SECRET: The client secret of the Reddit application.
     - REDDIT_USER_NAME: The username of the Reddit user.
     - REDDIT_USER_PASSWORD: The password of the Reddit user.
+    - SCREEN_HEIGHT: The height of the screen in pixels.
+    - SCREEN_WIDTH: The width of the screen in pixels.
+    - PYTHONWARNINGS: The warnings to be ignored.
+    - TOKENIZERS_PARALLELISM: The parallelism of the tokenizers.
     """
 
     # Main video settings
