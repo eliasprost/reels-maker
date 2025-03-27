bcbfc974dc4a46b0245a4a4e45a8814e32836a3b
Elias Prost <elias.prost@gmail.com>
2025-03-26 22:19:27 -0300

    remove unused path function

--- src/utils/path.py
+++ src/utils/path.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
-from typing import List
 
 from loguru import logger
 
@@ -18,14 +17,3 @@ def create_file_folder(file_path: str) -> None:
         logger.info(f"Folder not found. Created folder: {path.parent}")
     else:
         logger.info(f"Folder already exists: {path.parent}")
-
-
-def get_folder_files(directory: str, extension: str) -> List[str]:
-    """
-    Get all files with a specific extension from a directory.
-
-    Args:
-        directory (str): The directory to search.
-        extension (str): The file extension to filter by.
-    """
-    return [f.name for f in directory.glob(f"*.{extension}")]
