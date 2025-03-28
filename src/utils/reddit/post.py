7675b9c623871985990e050891b2333222271ad0
Elias Prost <elias.prost@gmail.com>
2025-03-28 01:49:33 -0300

    refactor parse function

--- src/utils/reddit/post.py
+++ src/utils/reddit/post.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import praw
+import re
 
+import praw
+from loguru import logger
+
+from config import settings
 from schemas import RedditComment, RedditPost
 
 
@@ -22,33 +26,49 @@ def parse_comment_permalink(comment: praw.models.Comment, post_id: str) -> str:
     )
 
 
-def parse_reddit_post(thread: praw.models.Submission) -> RedditPost:
+def get_reddit_object(url: str) -> RedditPost:
     """
-    Parse a Reddit thread to a RedditPost object
+    Parse a Reddit thread URL to a RedditPost object
 
     Args:
-        thread: praw.models.Submission
+        url: Reddit thread URL
     """
 
-    return RedditPost(
-        post_id=thread.id,
-        title=thread.title,
-        body=thread.selftext,
-        comments=[
-            RedditComment(
-                comment_id=comment.id,
-                post_id=thread.id,
-                body=comment.body,
-                author=comment.author.name,
-                score=comment.score,
-                permalink=parse_comment_permalink(comment, thread.id),
-            )
-            for comment in thread.comments
-            if comment.body != "[deleted]" and comment.author
-        ],
-        num_comments=thread.num_comments,
-        tag=thread.link_flair_text,
-        author=thread.author.name,
-        score=thread.score,
-        permalink=thread.permalink,
-    )
+    if re.match(settings.REDDIT_PATTERN, url):
+
+        reddit = praw.Reddit(
+            client_id=settings.REDDIT_CLIENT_ID,
+            client_secret=settings.REDDIT_CLIENT_SECRET,
+            user_agent="Accessing Reddit threads",
+            username=settings.REDDIT_USER_NAME,
+            passkey=settings.REDDIT_USER_PASSWORD,
+            check_for_async=False,
+        )
+        thread = reddit.submission(url=url)
+
+        return RedditPost(
+            post_id=thread.id,
+            title=thread.title,
+            body=thread.selftext,
+            comments=[
+                RedditComment(
+                    comment_id=comment.id,
+                    post_id=thread.id,
+                    body=comment.body,
+                    author=comment.author.name,
+                    score=comment.score,
+                    permalink=parse_comment_permalink(comment, thread.id),
+                )
+                for comment in thread.comments
+                if comment.body != "[deleted]" and comment.author
+            ],
+            num_comments=thread.num_comments,
+            tag=thread.link_flair_text,
+            author=thread.author.name,
+            score=thread.score,
+            permalink=thread.permalink,
+        )
+
+    else:
+        logger.error(f"Invalid Reddit URL: {url}")
+        raise ValueError(f"Invalid Reddit URL: {url}")
